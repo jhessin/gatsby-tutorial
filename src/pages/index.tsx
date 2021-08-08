060ac4bad68280331bc638e47fb4368cf9f5fb49
Jim Hessin <jhessin@gmail.com>
2021-08-08 16:26:16 -0600

    step 2 restart and restyle

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () => {
+	return (
+		<Layout pageTitle="Home Page">
+			<h1>Welcome to my Gatsby site!</h1>
+			<p>I'm making this by following the Gatsby Tutorial.</p>
+		</Layout>
+	);
+};
